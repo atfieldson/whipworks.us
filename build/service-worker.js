50bf72f0e292e6ee1a7ba8b7bfdb20b913833c38
Adam Fieldson <atfieldson@gmail.com>
2018-12-30 12:31:20 -0600

    ran build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.975648a977b620b3fe42ecfd67eab7e8.js"
+  "/precache-manifest.64970ab61bc891ca62d1110012d137a1.js"
 );
 
 workbox.clientsClaim();
