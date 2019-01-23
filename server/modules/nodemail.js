9c85ade65a1cf0540b55b4b331edab3baede696f
Joseph Carey <josephcareycomposer@gmail.com>
2019-01-22 18:51:34 -0600

    add basic automatic email functionality

--- server/modules/nodemail.js
+++ server/modules/nodemail.js
@@ -1,41 +1,38 @@
 "use strict";
 const nodemailer = require("nodemailer");
 
-// async..await is not allowed in global scope, must use a wrapper
-async function nodemail(to, subject, message){
+let nodemailerPeon = {
+  sendEmail: async (to, subject, message) => {
 
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let account = await nodemailer.createTestAccount();
+    try {
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+               user: process.env.EMAIL_ADDRESS,
+               pass: process.env.EMAIL_PASSWORD
+           }
+       });
 
-  // atfieldson@gmail.com
-  
+       let mailOptions = {
+        from: '"Adam Fieldson" <atfieldson@gmail.com>', // sender address
+        to: to || "atfieldson@gmail.com", // list of receivers
+        cc: '"Adam Fieldson" <atfieldson@gmail.com>',
+        subject: subject || "Seems like something isn't working quite right with Nodemailer", // Subject line
+        text: message, // plain text body
+        // html: "<b>Hello world?</b>" // html body
+      };
+    
+      // send mail with defined transport object
+      let info = await transporter.sendMail(mailOptions)
+    
+      console.log("Email sent: %s", info.messageId);
 
-  // create the transporter
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-           user: 'atfieldson@gmail.com',
-           pass: 'knzu ialz qepj heuk'
-       }
-   });
+    } catch (error) {
+      console.log(error)
+    }
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: '"Adam Fieldson" <atfieldson@gmail.com>', // sender address
-    to: to, // list of receivers
-    cc: '"Adam Fieldson" <atfieldson@gmail.com>',
-    subject: subject, // Subject line
-    text: message, // plain text body
-    // html: "<b>Hello world?</b>" // html body
-  };
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail(mailOptions)
-
-  console.log("Email sent: %s", info.messageId);
+  }
 }
 
-nodemail().catch(console.error);
-
-module.exports = nodemail;
+module.exports = nodemailerPeon;
