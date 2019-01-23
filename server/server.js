7ea78f672bb2aa7a5026b5dda6322e2be7f3b8bd
Joseph Carey <josephcareycomposer@gmail.com>
2019-01-22 18:54:43 -0600

    add basic automatic email functionality

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-<<<<<<< HEAD
+
 const bodyParser = require("body-parser");
 const sessionMiddleware = require("./modules/session-middleware");
 
